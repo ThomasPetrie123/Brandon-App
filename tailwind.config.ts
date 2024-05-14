d81bef2df05723c63f7d57c33d628c2e411abf04
Brandon <brandon.toferrett@gmail.com>
2024-05-14 20:37:58 +0100

    some tings

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/main.tsx",
+    "./src/App.tsx",
+    "./src/components/Message.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
