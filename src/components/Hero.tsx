aed2228b60ad0bca5c6dc960402d4fd6d22ba73c
Brandon <brandon.toferrett@gmail.com>
2024-05-14 23:33:33 +0100

    Finished for the day

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,15 +4,15 @@ const Hero = () => {
   return (
     <div className="flex min-h-screen mx-auto items-center bg-base-200">
       <div className="w-full flex flex-col lg:flex-row justify-between items-center max-w-[80%] mx-auto">
-        <div className="">
-          <h1 className="text-7xl font-bold max-w-3xl">Hampshire Based Electrical Company</h1>
+        <div className="text-left sm:text-center md:text-left">
+          <h1 className="text-5xl lg:text-7xl font-bold max-w-3xl">Hampshire Based Electrical Company</h1>
           <p className="py-6 max-w-2xl text-lg">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           <div className="flex flex-col md:flex-row gap-3 lg:gap-4 mx-auto">
-            <button className="btn btn-primary w-full lg:w-36">Our Work<i className="fa-solid fa-arrow-right mt-[2px]"></i></button>
-            <button className="btn w-full lg:w-36 btn-outline">Contact Us</button>
+            <button className="btn btn-primary w-full md:w-36">Our Work<i className="fa-solid fa-arrow-right mt-[2px]"></i></button>
+            <button className="btn w-full md:w-36 btn-outline">Contact Us</button>
           </div>
         </div>
-        <img src={HeroImage} className="rounded-lg hidden lg:block max-w-xl" alt="Hero" />
+        <img src={HeroImage} className="rounded-lg hidden lg:block xl:max-w-xl lg:max-w-lg" alt="Hero" />
       </div>
     </div>
   )
